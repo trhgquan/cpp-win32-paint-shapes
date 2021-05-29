8821e9ce638a2f9e7ae16c3b60e71726c29e7766
trhgquan <quanthcstpbt@gmail.com>
2021-05-29 20:08:06 +0700

    Background menu caption changed

---
+++ Source/Paint/resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Paint.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PAINT_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PAINT                       107
+#define IDI_SMALL                       108
+#define IDC_PAINT                       109
+#define ID_FILE_NEW                     110
+#define ID_TOOLBAR                      111
+#define ID_FILE_SAVE                    112
+#define ID_FILE_OPEN                    113
+#define ID_DRAW_CIRCLE                  114
+#define ID_DRAW_ELLIPSE                 115
+#define ID_DRAW_SQUARE                  116
+#define ID_DRAW_RECTANGLE               117
+#define ID_DRAW_LINE                    118
+#define ID_DRAW_TEXT                    119
+#define ID_SHAPE_SELECT                 120
+#define ID_SHAPE_DELETE                 121
+#define ID_SHAPE_CUT                    122
+#define ID_SHAPE_COPY                   123
+#define ID_SHAPE_PASTE                  124
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     130
+#define IDC_CURSOR1                     133
+#define ID_CONFIG_COLOUR                32771
+#define ID_COLOUR_LINECOLOUR            32772
+#define ID_COLOUR_BACKGROUND            32773
+#define ID_COLOUR_LINE                  32774
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           125
+#endif
+#endif
